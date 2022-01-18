4d867e6d5efcd06326dfc4a0d04833b80a376214
David-CG <dacugam2000@gmail.com>
2022-01-18 17:55:04 +0100

    Clase 18/01

--- app/src/main/java/net/iessochoa/davidcuarterogambin/practica5/ui/EdicionDiaActivity.java
+++ app/src/main/java/net/iessochoa/davidcuarterogambin/practica5/ui/EdicionDiaActivity.java
@@ -1,16 +1,83 @@
 package net.iessochoa.davidcuarterogambin.practica5.ui;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import net.iessochoa.davidcuarterogambin.practica5.R;
 
+import java.util.Date;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class EdicionDiaActivity extends AppCompatActivity {
+
+    static final String EXTRA_DIA = "net.iessochoa.davidcuartero.practica5.EdicionDiaActivity.DiaCreado";
+
+    TextView tvFecha;
+    ImageView ivFecha;
+    EditText etResumen, etDiario;
+    Spinner spValoracion;
+    FloatingActionButton fabGuardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edicion_dia);
+
+        // Inicia las views de la activity
+        iniciaViews();
+
+        ivFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickFecha();
+            }
+        });
+
+        fabGuardar.setOnClickListener(view -> {
+            //getIntent().putExtra(EXTRA_DIA, (Parcelable) dia);
+        });
+    }
+
+    public void onClickFecha() {
+
+        Calendar newCalendar = Calendar.getInstance();
+
+        DatePickerDialog dialogo = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, monthOfYear, dayOfMonth);
+
+                EdicionDiaActivity.this.tvFecha.setText(dayOfMonth + "/" + monthOfYear + "/" + year);
+            }
+
+        }, newCalendar.get(Calendar.YEAR),
+                newCalendar.get(Calendar.MONTH),
+                newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        dialogo.show();
+    }
+
+    private void iniciaViews() {
+        tvFecha = findViewById(R.id.tvFecha);
+        ivFecha = findViewById(R.id.ivFecha);
+        etResumen = findViewById(R.id.etResumen);
+        etDiario = findViewById(R.id.etDiario);
+        spValoracion = findViewById(R.id.spValoracion);
+        fabGuardar = findViewById(R.id.fabGuardar);
     }
 }
--- app/src/main/java/net/iessochoa/davidcuarterogambin/practica5/ui/MainActivity.java
+++ app/src/main/java/net/iessochoa/davidcuarterogambin/practica5/ui/MainActivity.java
@@ -7,17 +7,27 @@ import android.view.Menu;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import net.iessochoa.davidcuarterogambin.practica5.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fabNuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicia las views de la activity
+        iniciaViews();
+
+        // Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fabNuevo.setOnClickListener(view -> {});
     }
 
     @Override
@@ -25,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void iniciaViews() {
+        fabNuevo = findViewById(R.id.fabNuevo);
     }
 }
