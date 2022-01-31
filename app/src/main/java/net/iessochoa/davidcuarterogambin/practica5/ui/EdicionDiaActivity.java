b16af8e56f321cea6702157c10fa3f01df0eb48a
David-CG <dacugam2000@gmail.com>
2022-01-31 20:47:53 +0100

    Clase 31/01/2022

--- app/src/main/java/net/iessochoa/davidcuarterogambin/practica5/ui/EdicionDiaActivity.java
+++ app/src/main/java/net/iessochoa/davidcuarterogambin/practica5/ui/EdicionDiaActivity.java
@@ -1,9 +1,11 @@
 package net.iessochoa.davidcuarterogambin.practica5.ui;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Build;
@@ -20,6 +22,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import net.iessochoa.davidcuarterogambin.practica5.R;
 import net.iessochoa.davidcuarterogambin.practica5.model.DiaDiario;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class EdicionDiaActivity extends AppCompatActivity {
@@ -68,7 +74,7 @@ public class EdicionDiaActivity extends AppCompatActivity {
                 Calendar calendar = Calendar.getInstance();
                 calendar.set(anyo, mes, dia);
 
-                EdicionDiaActivity.this.tvFecha.setText(dia + "/" + mes + "/" + anyo);
+                EdicionDiaActivity.this.tvFecha.setText(new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
             }
 
         }, newCalendar.get(Calendar.YEAR),
@@ -76,6 +82,42 @@ public class EdicionDiaActivity extends AppCompatActivity {
                 newCalendar.get(Calendar.DAY_OF_MONTH));
 
         dialogo.show();
+    }
+
+    public View.OnClickListener onClickGuardar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etResumen.setText(etResumen.getText().toString().trim());
+                etContenido.setText(etContenido.getText().toString().trim());
+                if ((etResumen.getText().toString().equals("")) || (etContenido.getText().toString().equals(""))) {
+                    dialogoCamposIncompletos();
+                } else {
+                    Intent resultado = new Intent();
+                    Date fecha = null;
+                    try {
+                        fecha = new SimpleDateFormat("dd/MM/yyyy").parse(tvFecha.getText().toString());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    resultado.putExtra(EXTRA_DIA, new DiaDiario(fecha, Integer.parseInt((String) spValoracion.getSelectedItem()), etResumen.getText().toString(), etContenido.getText().toString()));
+                    setResult(RESULT_OK, resultado);
+                    finish();
+                }
+            }
+        };
+    }
+
+    private void dialogoCamposIncompletos() {
+        AlertDialog.Builder dlgAcercaDe = new AlertDialog.Builder(this);
+        dlgAcercaDe.setTitle(R.string.titulo_campos_incompletos);
+        dlgAcercaDe.setMessage(R.string.cont_campos_incompletos);
+        dlgAcercaDe.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        dlgAcercaDe.show();
     }
 
     private void iniciaViews() {
